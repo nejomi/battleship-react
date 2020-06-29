19489f64ff8e8122d4afbdcb545e0a06ebd2d71b
blackstate <josemiguelmneyra@gmail.com>
2020-06-29 19:31:19 +0800

    Updates

---
+++ precache-manifest.66bcf57c5d2f5e3ea3b28a60dc69e5a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c138988772b9472ec4e704196b337b20",
+    "url": "/battleship-react/index.html"
+  },
+  {
+    "revision": "d59b0f6fb148c907d1a2",
+    "url": "/battleship-react/static/css/main.46b41484.chunk.css"
+  },
+  {
+    "revision": "312f3a45e0317145cc2b",
+    "url": "/battleship-react/static/js/2.752adc68.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/battleship-react/static/js/2.752adc68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d59b0f6fb148c907d1a2",
+    "url": "/battleship-react/static/js/main.dd6a9a25.chunk.js"
+  },
+  {
+    "revision": "571a2ae2df6bc3d8c81c",
+    "url": "/battleship-react/static/js/runtime-main.7585f23a.js"
+  }
+]);
